091bab4ddead9d65953d2ec6b5b3eb4e75ebaf12
Serdar Kilic <serdar@kilic.net>
2023-11-11 00:40:47 +1100

    wip

--- backend/src/plugins/slone/server/index.ts
+++ backend/src/plugins/slone/server/index.ts
@@ -1,9 +1,9 @@
-import register from './register';
-import bootstrap from './bootstrap';
-import destroy from './destroy';
+import register from "./register";
+import bootstrap from "./bootstrap";
+import destroy from "./destroy";
 
 export default {
   register,
   bootstrap,
-  destroy
+  destroy,
 };
